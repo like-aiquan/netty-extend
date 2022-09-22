dbc01f1aa03c73f217cd279428ce4e4572eefed3
chenaiquan <like.aiquan@qq.com>
2022-09-22 19:44:08 +0800

    pom support

--- reactor-netty-extend/src/main/java/like/ai/tcp/EchoServer.java
+++ reactor-netty-extend/src/main/java/like/ai/tcp/EchoServer.java
@@ -2,6 +2,7 @@ package like.ai.tcp;
 
 
 import java.nio.charset.Charset;
+import java.util.concurrent.atomic.AtomicReference;
 import reactor.core.publisher.Flux;
 import reactor.netty.tcp.TcpServer;
 
@@ -15,9 +16,9 @@ public class EchoServer {
 				.port(8080)
 				.wiretap(true)
 				.handle((in, out) -> {
-					// 订阅者必须等于 2 才开始订阅 .publish().autoConnect(2);
-					Flux<String> flux = in.receive().asString(Charset.defaultCharset()).publish().autoConnect(2);
-
+					Flux<String> flux = in.receive().asString(Charset.defaultCharset());
+					// 订阅者必须大于 2 才开始订阅
+					flux = flux.publish().autoConnect(2);
 					// 第一个订阅 去除 后置 换行符
 					flux.subscribe(message -> {
 						if (message.endsWith("\n")) {
@@ -26,8 +27,7 @@ public class EchoServer {
 						// 打印 message
 						System.out.println("message [" + message + "]");
 					});
-
-					// 把请求的 message 响应给客户端
+					// 把请求的 message 响应给客户端  flux 的 第二次订阅
 					return out.sendString(flux.map(item -> item)).neverComplete();
 				})
 				.bindNow()
